3a7b5de10a0d69d958dfb27039d3efc98355e3a2
Deepanshu Rathore <deepanshurathore@Deepanshus-MacBook-Air.local>
2025-03-22 06:11:54 +0530

    improved features

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,7 +15,7 @@ const HomePage = () => {
             
             <Features />
             
-            <Analysis />
+            {/* <Analysis /> */}
             <Faq />
             
             <Pricing />
