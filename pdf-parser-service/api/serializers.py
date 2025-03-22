e920c95497901bfa73c38e35a8215245ccfdfec0
Rishabh kumar patel <rishu050803@gmail.com>
2025-03-22 08:23:35 +0530

    dynamic ATS feature

--- pdf-parser-service/api/serializers.py
+++ pdf-parser-service/api/serializers.py
@@ -3,5 +3,5 @@ from .models import YourModelName  # Replace with your actual model name
 
 class YourModelSerializer(serializers.ModelSerializer):
     class Meta:
-        model = YourModelName  # Replace with your actual model name
+        model = YourModelName  # Replace with your actual 
         fields = '__all__'  # Specify the fields you want to include in the serialization
